5f06ba25f649fe48d74cc7c01eead48f58ede9df
pikachudratini <andrewailist@gmail.com>
2026-06-23 22:14:22 -0400

    Support subpath deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_BASE_PATH || '';
+
 const nextConfig = {
   experimental: {},
+  basePath,
 };
 
 export default nextConfig;
